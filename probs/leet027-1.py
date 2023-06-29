931913a8af770099d87e96654083c7ea1725b60b
mwprcvl <46305372+mwprcvl@users.noreply.github.com>
2023-06-29 13:18:53 -0500

    in place removal

---
+++ probs/leet027-1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env/python3
+
+"""
+Given an integer array nums and an integer val, remove all occurrences of val in
+ nums in-place. The order of the elements may be changed. Then return the number
+ of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get
+ accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements
+ which are not equal to val. The remaining elements of nums are not important as
+ well as the size of nums.
+
+Return k.
+"""
+
+
+class Solution:
+    """ simpler solution tracking index along list """
+
+    def removeElement(self, nums: list[int], val: int) -> int:
+        """ """
+        keep_idx, k = 0, 0
+        for ele in nums:
+            if ele != val:
+                nums[keep_idx] = ele
+                keep_idx += 1
+            else:
+                k += 1
+        nums = nums[:keep_idx]
+
+        return k
+
+
+sol = Solution()
+
+nums = [3,2,2,3]
+assert sol.removeElement(nums, 3) == 2
+assert nums[:2] == [2, 2]
+
+nums = [0,1,2,2,3,0,4,2]
+assert sol.removeElement(nums, 2) == 3
+assert nums[:5] == [0,1,3,0,4]
